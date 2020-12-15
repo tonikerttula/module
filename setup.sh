b416efe83b99aadecdafd3bba3641c375812abc4
tonikerttula <74555178+tonikerttula@users.noreply.github.com>
2020-12-15 17:51:39 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y 
+
+sudo apt-get openssh-server -y
+
+sudo apt-get install curl
+
+sudo apt-get install -y salt-master salt-minion
+
+sudo apt-get install -y git
+
+setxkbmap fi
